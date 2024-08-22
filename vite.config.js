08fc72f4c1f0e32a6e46ac3b34747d22fa422d3e
Volodymyr <Volodumurvel@gmail.com>
2024-08-22 19:13:46 +0200

    add terms

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { globalStylesOptions } from './global.styles';
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { globalStylesOptions } from "./global.styles";
 
 export default defineConfig(({ command }) => {
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    root: 'src',
+    root: "src",
     build: {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync("./src/*.html"),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              return "vendor";
             }
           },
-          entryFileNames: 'commonHelpers.js',
+          entryFileNames: "commonHelpers.js",
         },
       },
-      outDir: '../dist',
+      outDir: "../dist",
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(["./src/**/**.html"]),
       ViteImageOptimizer({
         exclude: /^sprite.svg$/,
         png: {
@@ -53,6 +53,6 @@ export default defineConfig(({ command }) => {
         },
       },
     },
-    base: '/vanilla-vite-template'
+    base: "/",
   };
 });
